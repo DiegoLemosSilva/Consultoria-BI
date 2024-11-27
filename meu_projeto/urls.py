8e7311e7a92a89670067093f235519dfc1321178
DiegoLemosSilva <57917626+DiegoLemosSilva@users.noreply.github.com>
2024-11-27 16:02:46 -0300

    Delete meu_projeto directory

--- meu_projeto/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py em meu_projeto
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cadastro_usuario.urls')),  # Inclui as URLs do app cadastro_usuario
-]
